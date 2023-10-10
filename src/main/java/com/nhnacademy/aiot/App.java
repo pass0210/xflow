39374755ed0b7a49a5d14413a146d4b67a8e8af0
pass0210 <pass0210@gmail.com>
2023-10-10 11:30:51 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/aiot/App.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.aiot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
