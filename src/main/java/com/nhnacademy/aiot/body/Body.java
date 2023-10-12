dae7b4c18343b3e71ea46ef2bbb9d2e3cd5644cc
pass0210 <pass0210@gmail.com>
2023-10-12 17:07:54 +0900

    Feat : Body 구현 완료

---
+++ src/main/java/com/nhnacademy/aiot/body/Body.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.aiot.body;
+
+public class Body {
+    private String data;
+
+    public Body(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
